f2a40f90cdd5c82202c0e57a0c5820b0489002c5
alikiminza <ali.kiminza@student.moringaschool.com>
2023-09-11 09:32:56 +0300

    changes made

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,74 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
+
+print(get_names(spicy_foods))
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+    for food in spicy_foods:
+        if food.get("heat_level", 0) > 5:
+            spiciest_foods.append(food)
+    return spiciest_foods
+
+print(get_spiciest_foods(spicy_foods))
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        cuisine = food.get("cuisine", "Unknown Cuisine")
+        heat_level = food.get("heat_level")
+        num_of_emojis = "🌶" * heat_level
+        food_info = f"{food['name']} ({cuisine}) | Heat Level: {num_of_emojis}"
+
+        print(food_info)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food.get("cuisine") == cuisine:
+            return food
+        
+matching_cuisine = get_spicy_food_by_cuisine(spicy_foods, "American")
+print(matching_cuisine)
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food.get("heat_level", 0) > 5:
+            heat_level_emojis = "🌶" * food["heat_level"]
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
+
+print_spiciest_foods(spicy_foods)
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    num_foods = len(spicy_foods)
+    total_heat_lvl = 0
+
+    for food in spicy_foods:
+        total_heat_lvl += food.get("heat_level")
+
+    if num_foods > 0:
+        average_heat_lvl = total_heat_lvl / num_foods
+        return average_heat_lvl
+    else:
+        return 0
+    
+averaged_heat_lvl = get_average_heat_level(spicy_foods)
+print(f"Avg: {averaged_heat_lvl:2f}")
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_spicy_foods = spicy_foods.copy()
+    new_spicy_foods.append(spicy_food)
+    return new_spicy_foods
+
+create_a_spicy_food = create_spicy_food(
+    spicy_foods,
+    spicy_food={
+        "name": "Griot",
+        "cuisine": "Haitian",
+        "heat_level": 10,
+    },
+)
+print(create_a_spicy_food)
